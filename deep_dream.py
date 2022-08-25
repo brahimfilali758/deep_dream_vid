99a0bcf812d113d20fec911d1d71e2a60477ea93
i.filali <filali.idetec@gmail.com>
2022-08-25 16:25:30 +0200

    changing DeepDream fonctinons to use only object propreties

--- deep_dream.py
+++ deep_dream.py
@@ -16,18 +16,19 @@ class DeepDream():
 		if not step_size :
 			self.step_size = 0.01
 		else : self.step_size = step_size
-
+		self.img = preprocess(self.img)
+		self.img = tf.convert_to_tensor(self.img)
 		print("The model summary is : \n")
 		print(self.model.summary())
 
 	def set_img(self,new_image):
 		self.img = new_image
 
-	def calc_loss(self,img, model):
+	def calc_loss(self):
 		# Pass forward the image through the model to retrieve the activations.
 		# Converts the image into a batch of size 1.
-		img_batch = tf.expand_dims(img, axis=0)
-		layer_activations = model(img_batch)
+		img_batch = tf.expand_dims(self.img, axis=0)
+		layer_activations = self.model(img_batch)
 		if len(layer_activations) == 1:
 			layer_activations = [layer_activations]
 
@@ -38,33 +39,37 @@ class DeepDream():
 
 		return  tf.reduce_sum(losses)
 
-	def optimize(self,img, steps, step_size):
+	def optimize(self):
 		loss = tf.constant(0.0)
 		for n in tf.range(self.steps):
 			print(f"Proceeding with step num : {n}")
 			with tf.GradientTape() as tape:
+				img_tensor = tf.convert_to_tensor(self.img)
 				# `GradientTape` only watches `tf.Variable`s by default
-				tape.watch(img)
-				loss = self.calc_loss(img, self.model)
-			# print(f"loss calculated succefully, loss = {loss}")	
+				tape.watch(img_tensor)
+				loss = self.calc_loss()
+			print(f"loss calculated succefully, loss = {loss}")	
 			# Calculate the gradient of the loss with respect to the pixels of the input image.
-			gradients = tape.gradient(loss, img)
-
+			gradients = tape.gradient(loss, img_tensor)
+			print(gradients)
 			# Normalize the gradients.
 			gradients /= tf.math.reduce_std(gradients) + 1e-8 
 
 			# In gradient ascent, the "loss" is maximized so that the input image increasingly "excites" the layers.
-			# You can update the image by directly adding the gradients (because they're the same shape!)
-			img = img + gradients*self.step_size
+			# We can update the image by directly adding the gradients (because they're the same shape!)
+			img = img_tensor + gradients*tf.convert_to_tensor(self.step_size)
 			img = tf.clip_by_value(img, -1, 1)
 			# print(f"Done with step num : {n}")	
 		return loss, img
 
+	def run_deep_dream(self):
+		pass
+		
 	def __call__(self) :
-		img = preprocess(self.img)
-		img = tf.convert_to_tensor(img)
-		step_size = tf.convert_to_tensor(self.step_size)
-		loss , img = self.optimize(img,self.steps,step_size)
+		# img = preprocess(self.img)
+		# img = tf.convert_to_tensor(img)
+		# step_size = tf.convert_to_tensor(self.step_size)
+		loss , img = self.optimize()
 		result = deprocess(img)
 		return result
 
